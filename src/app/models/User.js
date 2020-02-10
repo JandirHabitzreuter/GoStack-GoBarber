c34433b97070b26afe7d66dace2bec1289b5466e
Jandir Habitzreuter <dyjandir@hotmail.com>
2020-02-10 06:47:01 -0300

    Criando criptografia com bcryptjs

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,13 @@
 const { Model } = require('sequelize');
 const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 class User extends Model{
     static init(sequelize){
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password : Sequelize.VIRTUAL,
             password_hash: Sequelize.STRING,
             provider: Sequelize.BOOLEAN,
 
@@ -15,6 +17,14 @@ class User extends Model{
         }
 
         );
+        //Vai ser executado antes do usuario ser criado
+        this.addHook('beforeSave', async (user) => {
+         if(user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8);
+         }
+
+        });
+      return this;
     }
 
 
