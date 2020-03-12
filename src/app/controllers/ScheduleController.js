453643bdc03345fcf82d9ae320b416319f902684
Jandir <dyjandir@hotmail.com>
2020-03-11 23:37:45 -0300

    Criando paginação e listagem

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -2,36 +2,40 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import *  as Yup from 'yup';
+import { startOfDay, endOfDay, parseISO} from 'date-fns';
+import {Op} from 'sequelize';
 
-import {startOfHour, parseISO, isBefore} from 'date-fns';
 
-class AppointmenteController {
+class ScheduleController {
     // no attributes sempre tem que retornar o Id   
 
     async index(req, res){
-        const { page = 1} = req.query;
-        const appointments = await Appointment.findAll({
-            where: {user_id: req.userId, canceled_at:null},
-            order :['date'],
-            attributes:['id', 'date'],
-            // listar 20 registros
-            limit:20,
-            // Quantos registros vai pular
-            offset: (page - 1) * 20,
-            include:[{
-                model:User,
-                as:'provider',
-                attributes:['id', 'name'],
-                include:[{
-                    model : File,
-                    as:'avatar',
-                    attributes:['id', 'path', 'url'],
-                }],
-            }                
-            ],            
-          
-    
-        });
+        
+       const checkUserProvider = await User.findOne(
+        {where : {id : req.userId, provider:true},        
+        }
+       );
+
+       if (!checkUserProvider){
+        return res.status(401).json({error: 'User is not provider.'});
+       }
+
+       const { date } = req.query;
+       const parsedDate = parseISO(date);
+
+       const appointments = await Appointment.findAll({
+        where: {
+            provider_id:req.userId,
+            canceled_at:null,
+            date:{
+                [Op.between]: [
+                    startOfDay(parsedDate),
+                    endOfDay(parsedDate),                                
+                ]
+            },
+        },
+        order: ['date'],
+       });
     
         return res.json(appointments);
     
@@ -94,4 +98,4 @@ class AppointmenteController {
 }
 
 
-export default new AppointmenteController();
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ import sessionController from './app/controllers/SessionController';
 import fileController from './app/controllers/FileController'
 import providerController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+
+
 
 import authMiddleware from './app/middleware/auth';
 import multerConfig from './config/multer';
@@ -25,6 +28,8 @@ routes.post('/files', upload.single('file'), fileController.store);
 routes.get('/providers', providerController.index);
 
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedules', ScheduleController.index);
 
 routes.post('/appointments', AppointmentController.store);
 
